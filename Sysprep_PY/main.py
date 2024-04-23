114fcb4e64d2fbeb5e595cd23dacbdff4344ceb9
Eguis Suarez <58845596+Eguis16@users.noreply.github.com>
2024-04-23 17:26:21 -0400

    Add files via upload

---
+++ Sysprep_PY/main.py
@@ -0,0 +1,9 @@
+#Implementacion de Sysprep para personalizacion de imagen
+#Codigo creado por Eguis Suarez - Especialista TI Fcom Spa
+
+import os
+import subprocess
+from Bloatware import Bloatware
+
+for element in Bloatware:
+    print(element)
